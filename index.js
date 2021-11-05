1140e2619036d3dd9110d0a3197f9bfc82f6ad03
Mario González <mario@mariogl.com>
2021-11-05 15:22:24 +0100

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const connectDB = require("./database/index");
 const initializeServer = require("./server/index");
 
 const port = process.env.PORT ?? process.env.SERVER_PORT ?? 5000;
-
+console.log("PORT", port);
 (async () => {
   try {
     await connectDB();
