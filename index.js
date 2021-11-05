c1a678afe3a62c9b8fdc4b24a361e6122bc127c2
Mario González <mario@mariogl.com>
2021-11-05 15:17:22 +0100

    Added PORT environment variable for deploying to Heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const connectDB = require("./database/index");
 
 const initializeServer = require("./server/index");
 
-const port = process.env.SERVER_PORT ?? 5000;
+const port = process.env.PORT ?? process.env.SERVER_PORT ?? 5000;
 
 (async () => {
   try {
